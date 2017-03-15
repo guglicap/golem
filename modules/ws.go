16420066ca35fc0fddc2c70b0fb38ee893f2dbc8
Goo <guglielmocaprotti@gmail.com>
2017-03-15 18:01:08 +0000

    Added i3 support

--- modules/ws.go
+++ modules/ws.go
@@ -1,65 +1,39 @@
 package modules
 
-import (
-	"bufio"
-	"encoding/json"
-	"errors"
-	"os/exec"
-	"regexp"
-	"unicode"
-)
+import "encoding/json"
 
 //WsModule is a workspace indicator for bspwm.
 type WsModule struct {
 	ModuleBase
 	WsFocused, WsUnfocused string
+	Wm                     string
 }
 
 //BuildWs initializes a WsModule
 func BuildWs(ms *ModuleSpec) Module {
 	opts := struct {
 		WsFocused, WsUnfocused string
+		Wm                     string
 	}{
 		WsFocused:   "\uf111",
 		WsUnfocused: "\uf10c",
+		Wm:          "bspwm",
 	}
 	json.Unmarshal([]byte(ms.Options), &opts)
 	return &WsModule{
 		buildModuleBase(ms),
 		opts.WsFocused,
 		opts.WsUnfocused,
+		opts.Wm,
 	}
 }
 
 //Run starts the module.
 func (m *WsModule) Run() {
-	re := regexp.MustCompile("([oOuUfF]\\d)")
-	if ok := inPATH("bspc"); !ok {
-		m.errOutput(errors.New("Can't find bspc in $PATH"))
-		return
+	switch m.Wm {
+	case "bspwm":
+		m.bspwm()
+	case "i3":
+		m.i3()
 	}
-	cmd := exec.Command("bspc", "subscribe")
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		m.errOutput(err)
-		return
-	}
-	scan := bufio.NewScanner(stdout)
-	err = cmd.Start()
-	result := " "
-	for scan.Scan() {
-		bspc := scan.Text()
-		matches := re.FindAllStringSubmatch(bspc, -1)
-		for _, match := range matches {
-			if unicode.IsUpper(rune(match[1][0])) {
-				result += m.WsFocused
-			} else {
-				result += m.WsUnfocused
-			}
-			result += " "
-		}
-		output <- m.update(result)
-		result = " "
-	}
-
 }
